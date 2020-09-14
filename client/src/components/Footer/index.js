cc0df002ee0c9b5547deb13153d4a1047e6fe744
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-09-14 08:20:57 -0400

    footer logo

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBolt } from "@fortawesome/free-solid-svg-icons";
 import "./style.css";
-import logo from "../../media/redlogo.png"
+import logo from "../../media/blackwhitelogo.png"
 import dots from "../../media/dots.png"
 export default class Footer extends Component {
   constructor(props) {
