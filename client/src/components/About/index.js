e531377f057f4963cc268a762e6387b0ec61ce6b
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-10-05 10:36:49 -0400

    remove duplicate 'and'

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -38,7 +38,7 @@ export default class About extends Component {
                 <p>We are</p>
                 <img src={wordmarkWhite}></img>
             </div>
-            <div>Two creative spirits who love to make things, and make them look good. We have Tim, who has a timeless love for all things Vietnam, coffee, and and mid-century designs. And we have Ryan, who is the ultimate enthusiast, has traveled all over and is always looking for more, is passionate about people and seeing their dreams be fully realized. A former colleague of Ryan and Tim's would always say that Pho was just soup. The irony of Pho being 'just soup' started the bond, because to Ryan and Tim that couldn't be more ridiculous. Now Ryan and Tim are looking at brands as more than just brands... craving to change the landscape and make businesses more than they ever dreamed.</div>
+            <div>Two creative spirits who love to make things, and make them look good. We have Tim, who has a timeless love for all things Vietnam, coffee, and mid-century designs. And we have Ryan, who is the ultimate enthusiast, has traveled all over and is always looking for more, is passionate about people and seeing their dreams be fully realized. A former colleague of Ryan and Tim's would always say that Pho was just soup. The irony of Pho being 'just soup' started the bond, because to Ryan and Tim that couldn't be more ridiculous. Now Ryan and Tim are looking at brands as more than just brands... craving to change the landscape and make businesses more than they ever dreamed.</div>
           </div>
          
         </div>
