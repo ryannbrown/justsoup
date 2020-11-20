7585a16ca8bcf07bc559f7ff13151ef8575057df
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-11-20 10:55:47 -0500

    lodashed the sidenav for resized windows

--- client/src/components/SideNav/index.js
+++ client/src/components/SideNav/index.js
@@ -1,14 +1,6 @@
-import { Container, Nav, Button, Image, Row, Col } from "react-bootstrap";
-import Navbar from "react-bootstrap/Navbar";
 import React, { Component } from "react";
-// import logo from './logo.svg';
-// import HomeHero from "../../components/HomeHero/index"
 import "./style.css";
-import arrow from "../../media/white-arrow.png";
-import logo from "../../media/redlogo.png"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle, faCircleNotch } from "@fortawesome/free-solid-svg-icons";
-
+var _ = require("lodash");
 export default class SideNav extends Component {
   constructor(props) {
     super(props);
@@ -16,80 +8,84 @@ export default class SideNav extends Component {
     this.listener = null;
     this.state = {
       status: "top",
-      location:1
+      location: 1,
     };
   }
 
+  componentDidMount() {
+    var height = window.innerHeight - 200;
+    var height2 = height + height;
+    var height3 = height2 + height;
+    var height4 = height3 + height;
+    // console.log(height, height2, height3, height4)
+    // console.log("hey")
 
-    componentDidMount() {
+    window.addEventListener(
+      "resize",
+      _.debounce(() => {
+        height = window.innerHeight;
+        height2 = height + height;
+        height3 = height2 + height;
+        height4 = height3 + height;
+        console.log("resizing", height, height2, height3, height4);
+      }, 400)
+    );
 
+    document.addEventListener("scroll", () => {
+      console.log(window.scrollY);
 
-      const height = window.innerHeight - 200 ;
-      const height2 = height + height;
-      const height3 = height2 + height;
-      const height4 = height3 + height;
-      // console.log(height, height2, height3, height4)
-      // console.log("hey")
-
-      
-        document.addEventListener('scroll', () => {
-            console.log(window.scrollY)
-           
-        //   const isTop = window.scrollY < 100;
-          if (window.scrollY <= height ) {
-              this.setState({ location: 1 })
-          }
-          if (window.scrollY > height && window.scrollY < height2) {
-              this.setState({ location: 2 })
-          }
-          if (window.scrollY > height2 && window.scrollY < height3 ) {
-              this.setState({ location: 3 })
-          }
-          if (window.scrollY > height3 ) {
-              this.setState({ location: 4 })
-          }
-        });
+      //   const isTop = window.scrollY < 100;
+      if (window.scrollY <= height) {
+        this.setState({ location: 1 });
       }
-
-  componentDidUpdate() {
+      if (window.scrollY > height && window.scrollY < height2) {
+        this.setState({ location: 2 });
+      }
+      if (window.scrollY > height2 && window.scrollY < height3) {
+        this.setState({ location: 3 });
+      }
+      if (window.scrollY > height3) {
+        this.setState({ location: 4 });
+      }
+    });
   }
 
   render() {
     return (
-
-
-        // <div className="side-nav" style={{backgroundColor:'transparent'}}>
-        //     <div className="side-nav-div">
-        //     <a href="/#1"><FontAwesomeIcon style={{color:'#d2d2d2'}} className={this.state.location === 1 ? 'active-a' : 'inactive'} icon={faCircle}></FontAwesomeIcon></a>
-        //    <p className={this.state.location === 1 ? ' animate__animated side-nav-text-active-a animate__bounceInLeft' : 'side-nav-text'}>Just Soup</p>
-        //     </div>
-        //     <div className="side-nav-div">
-        //     <a href="/#2"><FontAwesomeIcon style={{color:'#d2d2d2'}} className={this.state.location === 2 ? 'active-b' : 'inactive'} icon={faCircle}></FontAwesomeIcon></a>
-        //     <p className={this.state.location === 2 ? ' animate__animated side-nav-text-active-b animate__bounceInLeft' : 'side-nav-text'}>Our Story</p>
-        //     </div>
-        //     <div className="side-nav-div">
-        //     <a href="/#3"> <FontAwesomeIcon style={{color:'#d2d2d2'}} className={this.state.location === 3 ? 'active-c' : 'inactive'} icon={faCircle}></FontAwesomeIcon></a>
-        //     <p className={this.state.location === 3 ? ' animate__animated side-nav-text-active-c animate__bounceInLeft' : 'side-nav-text'}>Work We have done</p>
-        //     </div>
-        //     <div className="side-nav-div">
-        //     <a href="/#4"><FontAwesomeIcon style={{color:'#d2d2d2'}} className={this.state.location === 4 ? 'active-d' : 'inactive'} icon={faCircle}></FontAwesomeIcon></a>
-        //     <p className={this.state.location === 4 ? ' animate__animated side-nav-text-active-d animate__bounceInLeft' : 'side-nav-text'}>Reach Out</p>
-        //     </div>
-        // </div>
-        <div className="side-nav" style={{display: 'none', backgroundColor:'transparent'}}>
-            <div className="side-nav-div">
-            <a href="/#1"><div className={this.state.location === 1 ? 'active-a' : 'inactive'}></div></a>
-            </div>
-            <div className="side-nav-div">
-            <a href="/#2"><div className={this.state.location === 2 ? 'active-b' : 'inactive'}></div></a>
-            </div>
-            <div className="side-nav-div">
-            <a href="/#3"> <div className={this.state.location === 3 ? 'active-c' : 'inactive'}></div></a>
-            </div>
-            <div className="side-nav-div">
-            <a href="/#4"><div className={this.state.location === 4 ? 'active-d' : 'inactive'}></div></a>
-            </div>
+      <div
+        className="side-nav"
+        style={{ display: "none", backgroundColor: "transparent" }}
+      >
+        <div className="side-nav-div">
+          <a href="/#1">
+            <div
+              className={this.state.location === 1 ? "active-a" : "inactive"}
+            ></div>
+          </a>
         </div>
+        <div className="side-nav-div">
+          <a href="/#2">
+            <div
+              className={this.state.location === 2 ? "active-b" : "inactive"}
+            ></div>
+          </a>
+        </div>
+        <div className="side-nav-div">
+          <a href="/#3">
+            {" "}
+            <div
+              className={this.state.location === 3 ? "active-c" : "inactive"}
+            ></div>
+          </a>
+        </div>
+        <div className="side-nav-div">
+          <a href="/#4">
+            <div
+              className={this.state.location === 4 ? "active-d" : "inactive"}
+            ></div>
+          </a>
+        </div>
+      </div>
     );
   }
 }
